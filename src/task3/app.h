a34e5ee9bb1c9b2793fd953831803a7fd8600b85
Nikita Krutoy <nikitakrutoy@mbp-nikita.local>
2022-04-09 14:36:46 +0300

    Reformat code

--- src/task3/app.h
+++ src/task3/app.h
@@ -1,9 +1,13 @@
 #pragma once
 
 bool app_init(int width, int height);
+
 bool app_should_close();
+
 void app_poll_events();
+
 void app_handle_resize(int &width, int &height);
+
 void app_terminate();
 
 typedef std::function<void(int, float, float)> mouse_cb_t;
